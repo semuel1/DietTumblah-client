7c1125e93fd31f0346c17133d5c87ca7f593595c
semuel1 <semuelst1@gmail.com>
2021-04-19 02:46:02 -0700

    oivn

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,5 +1,4 @@
 
-  
 import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
