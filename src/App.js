62ac7b10bd15d81cec23b3c41c85423600d62d37
semuel1 <semuelst1@gmail.com>
2021-04-19 02:12:35 -0700

    client

--- src/App.js
+++ src/App.js
@@ -1,69 +1,21 @@
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-} from "react-router-dom"
-import { useEffect, useState } from "react"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import SaveToken from "./pages/SaveToken"
-import jwt from 'jsonwebtoken'
-import NoMatch from "./pages/NoMatch"
-import Profile from "./pages/Profile"
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
 
-const App = () => {
-    const [user, setUser] = useState(null)
+const App = () => (
+  <BrowserRouter>
+    <Container maxWidth="lg">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+    </Container>
+  </BrowserRouter>
+);
 
-    // 'Login' the user from JWT if it exists in localStorage
-    useEffect(() => {
-        const token = localStorage.getItem('jwt')
-        try { 
-            if(token) {
-                const user = jwt.decode(token)
-                // console.log('Ther user from token', user)
-                setUser(user)
-            }
-        } catch(err) {
-            console.log(err)
-            console.log('the token is expired!')
-            localStorage.removeItem('jwt')
-            setUser(null)
-        }
-    }, [])
-
-    const handleLogout = () => {
-        if(localStorage.getItem('jwt')) {
-            localStorage.removeItem('jwt')
-            setUser(null)
-        }
-    }
-
-    return (
-        <Router>
-            <Navbar user={user} handleLogout={handleLogout} />
-            <div className="container">
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/login">
-                        <Login user={user} />
-                    </Route>
-                    <Route path="/saveToken">
-                        <SaveToken setUser={setUser} />
-                    </Route>
-                    <Route>
-                        <Profile user={user} />
-                    </Route>
-                    <Route path="*">
-                        <NoMatch />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    )
-}
-
-export default App
+export default App;
